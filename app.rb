8aabc92a8d568f7a0cdacc2b885156b6549b89ca
keighty <k80.leonard@gmail.com>
2013-07-10 11:14:52 -0700

    moves inheritance from config to app

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 
-configure do
-  # set :public_folder, Proc.new { File.join(root, "public") }
+Dir.glob("controllers/*.rb").each { |r| require_relative r }
+
+class JSGames < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/solitaire' do
+    erb :solitaire
+  end
+
+  get '/mastermind' do
+    erb :mastermind
+  end
 end
-
-get '/' do
-  erb :index
-end
-
-get '/solitaire' do
-  erb :solitaire
-end
-
-get '/mastermind' do
-  erb :mastermind
-end
-
