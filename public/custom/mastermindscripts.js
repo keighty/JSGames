fcf496ec3bd86e118656dc684812c8c328c39e89
keighty <k80.leonard@gmail.com>
2013-07-10 19:34:43 -0700

    adds styling for mastermind page

---
+++ public/custom/mastermindscripts.js
@@ -0,0 +1,10 @@
+$(function() {
+  var form = $('form');
+  form.hide();
+
+  $('.btn-primary').bind('click', function() {
+    form.show();
+    // create the secret code
+    // add enter guess and text field to template
+  });
+});
