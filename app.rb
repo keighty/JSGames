e59115893f1d5fc2d3191d978f7151bee7e324cb
keighty <k80.leonard@gmail.com>
2013-07-16 14:46:44 -0700

    begins journey to rubyland

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'sinatra/base'
-
-Dir.glob("controllers/*.rb").each { |r| require_relative r }
+# require 'lib/mastermind'
 
 class JSGames < Sinatra::Base
   get '/' do
@@ -15,4 +14,16 @@ class JSGames < Sinatra::Base
   get '/mastermind' do
     erb :mastermind
   end
+
+  post '/game/:guess' do
+    game = set_game
+    game.guess(params[:guess])
+  end
+
+# sinatra passes through if you tell it to
+  private
+    def set_game
+      @game = Mastermind::Game.start(params[:code])
+    end
+    # game = Mastermind::Game.start(session)
 end
