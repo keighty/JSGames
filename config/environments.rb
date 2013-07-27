b9e9da13294ece7deefb3a82afd3ca821d647f85
keighty <k80.leonard@gmail.com>
2013-07-27 15:41:43 -0700

    troubleshooting heroku

--- config/environments.rb
+++ config/environments.rb
@@ -1,20 +1,14 @@
-configure :development do
-  set :database, 'sqlite:///development.db'
-end
+require 'active_record'
+require 'uri'
 
-configure :test do
-  set :database, 'sqlite://test.db'
-end
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-  )
-end
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :port     => db.port,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
