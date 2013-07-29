4365049ff732e1d8e96568332d04ad0ab846ee7c
keighty <k80.leonard@gmail.com>
2013-07-29 16:41:11 -0700

    refactors solitaire javascript

--- public/custom/solitaire/script.js
+++ public/custom/solitaire/script.js
@@ -1,54 +1,21 @@
+var row = "<div class='row'></div>",
+    square = "<div class='span4 square'></div>",
+    victory = "<div class='victory'>You won!</div>",
+    gridSize = 5,
+    allSquares;
+
+
+
 $(function () {
-  var row = "<div class='row'></div>",
-      square = "<div class='span4 square'></div>",
-      victory = "<div class='victory'>You won!</div>",
-      gridSize = 5;
-
-  // make the grid
-    // make the rows
-    for(var i=0; i<gridSize; i++) {
-      $(row).prependTo(".gameGrid");
-    }
-
-    // make the squares
-    $.each($('.row'), function(index, value) {
-      for(var j=0; j<gridSize; j++) {
-        $(square).appendTo($(this));
-      }
-    });
-
-  var allSquares = $('.square');
-
-  // set the center square to blue
-  var setCenter = function(squares) {
-    var center = Math.floor(squares.length / 2);
-    allSquares.removeClass('blue');
-    $(squares[center]).addClass('blue');
-  };
-
+  makeGrid(gridSize);
+  allSquares = $('.square');
   setCenter(allSquares);
 
   $('.container').on('click', '.square', function() {
     var mySquare = $(this);
+    colorNeighbors(mySquare);
 
-    // toggle the blue class of the clicked square
-    mySquare.toggleClass('blue');
-
-    // toggle the class of the next door neighbours
-    mySquare.next().toggleClass('blue');
-    mySquare.prev().toggleClass('blue');
-      //get current square index
-    var myIndex = mySquare.index('div.square');
-      // add the gridsize and toggle class
-    $(allSquares[myIndex + gridSize]).toggleClass('blue');
-      // subtract the gridsize and toggle class
-    $(allSquares[myIndex - gridSize]).toggleClass('blue');
-
-    // check to see if all squares are blue
-    if (allSquares.length === $('.blue').length) {
-      // show victory sign
-      $(victory).prependTo(".container");
-    }
+    checkVictory(allSquares);
 
   });
 
@@ -57,3 +24,50 @@ $(function () {
     $(victory).empty();
   });
 });
+
+// make the game grid
+var makeGrid = function(size) {
+  // make the rows
+  for(var i=0; i<size; i++) {
+    $(row).prependTo(".gameGrid");
+  }
+
+  // make the squares
+  $.each($('.row'), function(index, value) {
+    for(var j=0; j<gridSize; j++) {
+      $(square).appendTo($(this));
+    }
+  });
+};
+
+var colorNeighbors = function(squareX) {
+     // toggle the blue class of the clicked square
+    squareX.toggleClass('blue');
+
+    // toggle the class of the nextdoor neighbors
+    squareX.next().toggleClass('blue');
+    squareX.prev().toggleClass('blue');
+    //get current square index
+    var myIndex = squareX.index('div.square');
+    // add the gridsize and toggle class
+    $(allSquares[myIndex + gridSize]).toggleClass('blue');
+    // subtract the gridsize and toggle class
+    $(allSquares[myIndex - gridSize]).toggleClass('blue');
+
+}
+
+// set the center square to blue
+var setCenter = function(squares) {
+  var center = Math.floor(squares.length / 2);
+  squares.removeClass('blue');
+  $(squares[center]).addClass('blue');
+};
+
+// check for victory
+var checkVictory = function checkVictory(squares) {
+  // check to see if all squares are blue
+  if (squares.length === $('.blue').length) {
+    // show victory sign
+    $(victory).prependTo(".container");
+  }
+};
