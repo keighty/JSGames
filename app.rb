ce0ce4e02da836286474b11deb980c2a93eff6b3
keighty <k80.leonard@gmail.com>
2013-07-29 17:21:15 -0700

    adds dependencies for quiz spec

--- app.rb
+++ app.rb
@@ -4,13 +4,17 @@ require './config/environments'
 
 require_relative 'routes/init'
 
+require './lib/quiz'
+require './models/quiz'
 require './models/question'
 
 class JSGames < Sinatra::Base
-  set :root, File.dirname(__FILE__)
+  get '/' do
+    erb :index
+  end
 
-  not_found do
-    halt 404, 'not found'
+  get '/solitaire' do
+    erb :'solitaire/index'
   end
 
 end
