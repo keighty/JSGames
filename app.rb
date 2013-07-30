e3e17fdfd216f6c4884341175e2f82aa857f22c1
keighty <k80.leonard@gmail.com>
2013-07-29 17:10:35 -0700

    adds question model to app

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments'
+
 require_relative 'routes/init'
+
+require './models/question'
 
 class JSGames < Sinatra::Base
   set :root, File.dirname(__FILE__)
