c2a0257747b3ef7012f8d77ca458510f25613b40
keighty <k80.leonard@gmail.com>
2013-07-29 17:21:42 -0700

    resets db with quiz schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130727161313) do
+ActiveRecord::Schema.define(version: 20130729234556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20130727161313) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "game"
+  end
+
+  create_table "quizzes", force: true do |t|
+    t.string   "question"
+    t.string   "yes"
+    t.string   "no"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
