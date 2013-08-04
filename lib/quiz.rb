1b7b6a0778301b3746b36ed3a92bd4725c295fc2
keighty <k80.leonard@gmail.com>
2013-08-04 11:28:11 -0700

    refines quiz for single use

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -1,52 +1,31 @@
+require 'json'
+
 module AnimalQuiz
   class Quizzer
-    attr_accessor :root, :current
+    attr_accessor :current
 
-    def self.start
+    def self.start(id)
       quiz = self.new
-      quiz.root = Quiz.find_by_id(1)
-      quiz.current = quiz.root
+      quiz.current = Quiz.find_by_id(id)
       return quiz
     end
 
     def ask
-      @current.question
-    end
-
-    def no
-      if @current.no.nil?
-        get_response
-      else
-        @current = Quiz.find_by_id(@current.no)
-        return @current.question
-      end
-    end
-
-    def yes
-      @current = Quiz.find_by_id(@current.yes)
-      return @current.question unless @current.nil?
-    end
-
-    def get_response
-      puts "What animal were you thinking of?"
-      animal = gets.chomp
-      puts "what question could I ask to distinguish #{animal} from #{@current.question}"
-      question = gets.chomp
-      puts "what is the answer to the question?"
-      answer = gets.chomp
-
-      wrong(animal, question, answer)
+      @current.to_json
     end
 
     def wrong(animal, question, answer)
       old_question = @current.question
+      question.titlecase!
+
+      question += '?' unless question.include?('?')
 
       if answer == 'yes'
-        yes = Quiz.create(question: "Is it a #{animal}")
+        yes = Quiz.create(question: "Is it a #{animal}?")
         no = Quiz.create(question: old_question)
       else
         yes = Quiz.create(question: old_question)
-        no = Quiz.create(question: "Is it a #{animal}")
+        no = Quiz.create(question: "Is it a #{animal}?")
       end
 
       @current.update_attributes(question: question, yes: yes.id, no: no.id)
